f279d71a90de6bc76c765488e220c26480e33e29
Mark van Seventer <mark@vseventer.com>
2013-07-25 14:23:24 -0400

    Updated license.

--- README.md
+++ README.md
@@ -25,3 +25,19 @@ The `scripts` directory contains application-specific files. These are:
 
 * `app.js` initializes Kinvey’s JavaScript library for use in your app. In addition, the application domain (classes `Book` and `BookCollection`) is specified.
 * `ui.js` connects the application domain with the user interface. This is done by fetching, saving, or removing books when certain events (for example: a button click) occur.
+
+## License
+
+    Copyright 2013 Kinvey, Inc.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- scripts/ui.js
+++ scripts/ui.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 Kinvey, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 $(function() {
   // Import declarations into current scope.
   var App = window.Bookshelf;
@@ -108,7 +123,7 @@ $(function() {
       return;
     }
     add.addClass('loading');
-    
+
     // Update status.
     status('info', 'Finding an empty shelf…');
 
@@ -157,7 +172,7 @@ $(function() {
       return;
     }
     button.addClass('loading');
-    
+
     // Update status.
     status('info', 'Looking for the shredder…');
 
